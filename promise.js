d51ef9232c1ed1fcf5382bc673bc42fa166a9c5a
Yoslie <yoslie.tan@gmail.com>
2022-06-12 07:14:15 +0700

    feat: update promiseOutput func using async await

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const dataIXX = await promiseTheaterIXX();
+  const dataVGC = await promiseTheaterVGC();
+  const data = dataIXX.concat(dataVGC);
+
+  let totalEmotion = 0;
+  data.map(item => item.hasil === emotion && (totalEmotion += 1) )
+  return totalEmotion
+};
 
 module.exports = {
   promiseOutput,
